b312ae0dd126e6b458fbd0456aa15adf3cff71cf
skygongque <1243650225@qq.com>
2022-09-01 22:11:11 +0800

    fixed tampermonkey script problem casused by api change

--- tampermonkeyScript/script.js
+++ tampermonkeyScript/script.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         微软tts 下载按钮
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  微软tts 下载按钮
+// @version      0.2.4
+// @description  微软tts 下载按钮 方便下载合成的音频
 // @author       skygongque
-// @match        https://azure.microsoft.com/zh-cn/services/cognitive-services/text-to-speech/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
+// @match        https://azure.microsoft.com/*/services/cognitive-services/text-to-speech/*
+// @icon         https://www.microsoft.com/favicon.ico
 // @grant        none
 // @run-at document-start
+// @license MIT
 // ==/UserScript==
 
 (function () {
@@ -33,10 +34,10 @@
                     }
                     var play = document.getElementById('playbtn'),
                         playli = document.getElementById('playli'),
-
+        
                         stop = document.getElementById('stopbtn'),
                         stopli = document.getElementById('stopli'),
-
+        
                         language = document.getElementById('languageselect'),
                         voice = document.getElementById('voiceselect'),
                         voicestyle = document.getElementById('voicestyleselect'),
@@ -44,31 +45,30 @@
                         roleplaydiv = document.getElementById('roleplayselectdiv'),
                         secondarylocale = document.getElementById('secondarylocaleselect'),
                         secondarylocalediv = document.getElementById('secondarylocaleselectdiv'),
-
+        
                         text = document.getElementById('ttstext'),
                         ssml = document.getElementById('ttsssml'),
                         status = document.getElementById('ttsstatus'),
-
+        
                         speed = document.getElementById('speed'),
                         speedlabel = document.getElementById('speedlabel'),
                         pitch = document.getElementById('pitch'),
                         pitchlabel = document.getElementById('pitchlabel'),
-
+        
                         player = null,
                         SpeechSDK = window.SpeechSDK,
                         voiceList = {},
                         styleList = {},
                         rolePlayList = {},
                         secondaryLocaleList = {},
-
+        
                         defaultText;
-
+        
                     SpeechSDK = window.SpeechSDK;
-
+        
                     $.ajax({
-                        url: 'https://' + localizedResources.region + '.tts.speech.microsoft.com/cognitiveservices/voices/list',
+                        url: 'https://' + localizedResources.region + '.api.speech.microsoft.com/cognitiveservices/voices/list',
                         type: 'GET',
-                        beforeSend: function textToSpeechVoiceListBeforeAjaxSend(xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + localizedResources.token); },
                         success: function textToSpeechVoiceListAjaxSuccess(data) {
                             // put neural voices in front.
                             var sorted = data.sort(function (a, b) {
@@ -104,26 +104,26 @@
                             global.Core.Util.TrackException('A Text To Speech voice list API Ajax error occurred: ' + error);
                         }
                     });
-
+        
                     function SpeakOnce() {
-                        var config = SpeechSDK.SpeechTranslationConfig.fromAuthorizationToken(localizedResources.token, localizedResources.region),
+                        var config = SpeechSDK.SpeechTranslationConfig.fromEndpoint(new URL('wss://' + localizedResources.region + '.api.speech.microsoft.com/cognitiveservices/websocket/v1?TrafficType=AzureDemo')),
                             synthesizer,
                             audioConfig;
-
+        
                         // due to a bug in Chromium (https://bugs.chromium.org/p/chromium/issues/detail?id=1028206)
                         // mp3 playback has some beeps, using a higher bitrate here as a workaround.
-                        config.speechSynthesisOutputFormat = SpeechSDK.SpeechSynthesisOutputFormat.Audio24Khz160KBitRateMonoMp3;
-
+                        config.speechSynthesisOutputFormat = SpeechSDK.SpeechSynthesisOutputFormat.Audio24Khz96KBitRateMonoMp3;
+        
                         player = new SpeechSDK.SpeakerAudioDestination();
                         player.onAudioEnd = function () {
                             stopli.hidden = true;
                             playli.hidden = false;
                         };
-
+        
                         audioConfig = SpeechSDK.AudioConfig.fromSpeakerOutput(player);
-
+        
                         synthesizer = new SpeechSDK.SpeechSynthesizer(config, audioConfig);
-
+        
                         synthesizer.synthesisCompleted = function () {
                             // 此时已经合成完毕了 arguments[1].privResult.privAudioData 为MP3文件
                             //debugger;
@@ -156,40 +156,45 @@
                             synthesizer.close();
                             synthesizer = null;
                         };
-
+        
                         synthesizer.SynthesisCanceled = function (s, e) {
                             var details;
-                            stopli.hidden = true;
-                            playli.hidden = false;
+                            stopPlaying();
                             details = SpeechSDK.CancellationDetails.fromResult(e);
                             if (details.reason === SpeechSDK.CancellationReason.Error) {
                                 status.innerText = localizedResources.srTryAgain;
                             }
                         };
-
+        
                         synthesizer.speakSsmlAsync(ssml.value, function () { }, function (error) {
                             status.innerText = localizedResources.srTryAgain + ' ' + error;
                         });
                     }
-
-                    stop.onclick = function () {
+        
+                    function stopPlaying() {
                         playli.hidden = false;
                         stopli.hidden = true;
-
+        
                         if (player !== null) {
                             player.pause();
                         }
-
+        
                         player = null;
+                        play.focus();
+                    }
+        
+                    stop.onclick = function () {
+                        stopPlaying();
                     };
-
+        
                     play.onclick = function () {
                         stopli.hidden = false;
                         playli.hidden = true;
                         status.innerText = '';
                         SpeakOnce(function () { });
+                        stop.focus();
                     };
-
+        
                     language.onchange = function () {
                         defaultText = '';
                         $.each(localizedResources.ttsDefaultText, function (key, value) {
@@ -203,24 +208,24 @@
                             defaultText = localizedResources.ttsDefaultText.default;
                         }
                         text.value = defaultText;
-
-                        // Arabic, Hebrew, Urdu and Persian use right-to-left text direction
+        
+                        // Arabic, Hebrew, Urdu, Persian and Pashto use right-to-left text direction
                         if (['ar', 'he', 'ur', 'fa', 'ps'].includes(language[language.selectedIndex].value.slice(0, 2))) {
                             text.style.direction = 'rtl';
                         } else {
                             text.style.direction = '';
                         }
-
+        
                         voice.innerHTML = voiceList[language[language.selectedIndex].value];
                         voice.onchange();
                         BuildAndSetSSML();
                     };
-
-
+        
+        
                     voice.onchange = function () {
                         voicestyle.innerHTML = '';
                         voicestyle.options.add(new Option('General', 'general'));
-
+        
                         if (styleList[voice[voice.selectedIndex].value]) {
                             $.each(styleList[voice[voice.selectedIndex].value], function (_index, element) {
                                 voicestyle.options.add(new Option(
@@ -232,7 +237,7 @@
                         } else {
                             voicestyle.disabled = true;
                         }
-
+        
                         if (rolePlayList[voice[voice.selectedIndex].value]) {
                             roleplay.innerHTML = '';
                             roleplay.options.add(new Option('Default', 'Default'));
@@ -243,7 +248,7 @@
                         } else {
                             roleplaydiv.hidden = true;
                         }
-
+        
                         if (secondaryLocaleList[voice[voice.selectedIndex].value]) {
                             secondarylocale.innerHTML = '';
                             secondarylocale.options.add(new Option(language[language.selectedIndex].text + ' - Default', language[language.selectedIndex].value));
@@ -256,21 +261,21 @@
                         } else {
                             secondarylocalediv.hidden = true;
                         }
-
+        
                         pitch.disabled = false;
                         speed.disabled = false;
-
+        
                         BuildAndSetSSML();
                     };
-
+        
                     voicestyle.onchange = function () {
                         BuildAndSetSSML();
                     };
-
+        
                     roleplay.onchange = function () {
                         BuildAndSetSSML();
                     };
-
+        
                     secondarylocale.onchange = function () {
                         if (secondarylocale.selectedIndex !== 0) {
                             pitch.disabled = true;
@@ -279,7 +284,7 @@
                             pitch.disabled = false;
                             speed.disabled = false;
                         }
-
+        
                         defaultText = '';
                         $.each(localizedResources.ttsDefaultText, function (key, value) {
                             if (secondarylocale[secondarylocale.selectedIndex].value === key) {
@@ -292,10 +297,10 @@
                             defaultText = localizedResources.ttsDefaultText.default;
                         }
                         text.value = defaultText;
-
+        
                         BuildAndSetSSML();
                     };
-
+        
                     pitch.oninput = function () {
                         var normalizedVal = ((pitch.value - pitch.min) / (pitch.max - pitch.min)) * (2),
                             displayVal;
@@ -306,11 +311,11 @@
                         }
                         pitchlabel.innerText = localizedResources.ttsPitch + ': ' + displayVal;
                     };
-
+        
                     pitch.onchange = function () {
                         BuildAndSetSSML();
                     };
-
+        
                     speed.oninput = function () {
                         var normalizedVal = ((speed.value - speed.min) / (speed.max - speed.min)) * (3),
                             displayVal;
@@ -321,27 +326,31 @@
                         }
                         speedlabel.innerText = localizedResources.ttsSpeed + ': ' + displayVal;
                     };
-
+        
                     speed.onchange = function () {
                         BuildAndSetSSML();
                     };
-
-                    text.onchange = function () {
+        
+                    text.oninput = function () {
                         BuildAndSetSSML();
                     };
-
+        
+                    ssml.oninput = function () {
+                        checkSSMLLength();
+                    };
+        
                     function BuildAndSetSSML() {
                         var ssmlHeader = '<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US">{0}</speak>',
                             ssmlVoiceName = '<voice name="{VOICE}">{0}</voice>',
                             ssmlProperties = '<prosody rate="{SPEED}" pitch="{PITCH}">{TEXT}</prosody>',
                             ssmlExpression = '<mstts:express-as {STYLE_ATTRIBUTE} {ROLE_PLAY_ATTRIBUTE}>{0}</mstts:express-as>',
                             ssmlSecondaryLocale = '<lang xml:lang="{SECONDARY_LOCALE}">{0}</lang>',
-
+        
                             ssmlText = ssmlProperties,
                             escapedText;
                         ssmlText = ssmlText.replace('{SPEED}', speed.value + '%');
                         ssmlText = ssmlText.replace('{PITCH}', pitch.value + '%');
-
+        
                         if (voicestyle.selectedIndex !== 0 || (!roleplaydiv.hidden && roleplay.selectedIndex !== 0)) {
                             ssmlText = ssmlExpression.replace('{0}', ssmlText);
                             if (voicestyle.selectedIndex !== 0) {
@@ -355,21 +364,34 @@
                                 ssmlText = ssmlText.replace('{ROLE_PLAY_ATTRIBUTE}', '');
                             }
                         }
-
+        
                         ssmlText = ssmlVoiceName.replace('{0}', ssmlText);
                         ssmlText = ssmlText.replace('{VOICE}', voice[voice.selectedIndex].value);
-
+        
                         escapedText = text.value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;')
                             .replace(/'/g, '&apos;');
-
+        
                         if (!secondarylocalediv.hidden) {
                             escapedText = ssmlSecondaryLocale.replace('{0}', escapedText);
                             escapedText = escapedText.replace('{SECONDARY_LOCALE}', secondarylocale[secondarylocale.selectedIndex].value);
                         }
                         ssmlText = ssmlHeader.replace('{0}', ssmlText);
                         ssmlText = ssmlText.replace('{TEXT}', escapedText);
-
+        
                         ssml.value = ssmlText;
+                        checkSSMLLength();
+                    }
+        
+                    function checkSSMLLength() {
+                        var ssmlText = ssml.value,
+                            ssmlLength = ssmlText.length;
+                        if (ssmlLength > 1000) {
+                            status.innerText = localizedResources.ttsTextTooLong;
+                            play.setAttribute('disabled', 'disabled');
+                        } else {
+                            status.innerText = '';
+                            play.removeAttribute('disabled');
+                        }
                     }
                 });
             };
